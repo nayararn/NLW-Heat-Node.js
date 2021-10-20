3d3549a0d3aa86542250dc0951f78ddc1ec8d645
nrodrigues <nrodrigues@lumis.com.br>
2021-10-19 22:38:37 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageController';
+import { ensureAuthenticate } from './middleware/ensureAuthenticate';
+
+const router = Router();
+
+router.post('/authenticate', new AuthenticateUserController().handle);
+
+router.post('/messages', ensureAuthenticate, new CreateMessageController().handle);
+
+export { router };
